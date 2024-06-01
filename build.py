703abea26d7e33e68f624dc53879d72621d03a35
Dr-Lego <Dr_Lego@ist-einmalig.de>
2024-06-01 15:35:42 +0200

    preparing save compression

--- build.py
+++ build.py
@@ -325,6 +325,26 @@ def refresh_data():
         meta,
         plaintext
     )
+    
+    
+    
+def compress_save(data: str) -> dict:
+    save: dict[str, list[dict]] = json.loads(data)
+    icons = {
+        "": None,
+        "assets/icons/person.png": 1,
+        "assets/icons/bomb.png": 2,
+        "assets/icons/state.png": 3,
+        "assets/icons/city.png": 4,
+    }
+
+
+    nodes = [list(filter(None, [node["id"], node["size"], node["x"], node["y"], icons[node.get("image", "")]])) for node in save["nodes"]]
+    ids = {node[0]: i+1 for i, node in enumerate(nodes)}
+
+    edges = [list(filter(None, [ids[edge["from"]], ids[edge["to"]], 1 if edge["arrows"] == "to" else None])) for edge in save["edges"]]
+    
+    return {"nodes": nodes, "edges": edges}
 
 
 class wait_for_stabilized(object):
