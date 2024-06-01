16cd5c4c9be4a9219b850cc1bca0f402aae5193b
Dr-Lego <Dr_Lego@ist-einmalig.de>
2024-06-01 10:35:59 +0200

    better build

--- minimize_build.py
+++ minimize_build.py
@@ -1,19 +1,43 @@
 import os
 import subprocess
+import re
 from pathlib import Path
 
-files = [val for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk('./visualize')] for val in sublist]
+exclude = ["./visualize/data/DATA.js", ".visualize/assets/standard_icons"]
 
+files = [val.replace("./visualize", "./build") for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk('./visualize')] for val in sublist if not [1 for e in exclude if val.startswith(e)]]
+
+#create directory tree
 for path in files:
     dir_path = os.path.dirname(path)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    open(path, 'w').close() # Create an empty file
+    os.makedirs(dir_path, exist_ok=True)
 
 for file in files:
-    if file.endswith(".js"):
-        with open("build/js/script.js", "w") as f:
-            f.write(subprocess.check_output(["terser", "./visualize/js/script.js", "--c", "--m"]).decode().strip("\n"))
-            f.close()
-    
-        print(subprocess.check_output(["roadroller", "./visualize/js/script.js"]))
+    original = file.replace("./build", "./visualize")
+    if not ".min." in file and os.path.getsize(original) < 10**6:
+        if file.endswith(".js"):
+            continue
+            print(file)
+            terser = subprocess.check_output(["terser", original, "--c", "--m"]).decode().strip("\n")
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(terser)
+                f.close()
+            previous_size = os.path.getsize(file)
+            subprocess.call(["roadroller", file, "-o", file], stdout=subprocess.DEVNULL)
+            if os.path.getsize(file) > previous_size:
+                with open(file, "w", encoding="utf-8") as f:
+                    f.write(terser)
+                    f.close()
+                    
+        elif file.endswith(".html"):
+            text = re.sub(r"(<!--[^-]*-->|\s|\n)+", r" ", open(original, "r", encoding="utf-8").read())
+            text = re.sub(r" (?=<|$)|<\/[tl].>|<.p> *(<[p\/])| ?\/?(>)", r"\1\2", text, flags=re.IGNORECASE)
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(text)
+                f.close()
+                
+        elif file.endswith(".css"):
+            text = re.sub(r"(\/\*[^\*]+?\*\/|\s)+", r" ", open(original, "r", encoding="utf-8").read())
+            with open(file, "w", encoding="utf-8") as f:
+                f.write(text)
+                f.close()
