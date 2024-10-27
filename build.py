57849556f462c9b0d873fa2d9fd1147a4803d94e
Dr-Lego <Dr_Lego@ist-einmalig.de>
2024-10-27 09:54:39 +0100

    fixed smalll preload bug

--- build.py
+++ build.py
@@ -389,7 +389,7 @@ def refresh_data():
 
     with open("frontend/src/lib/data/data.js", "w", encoding="utf-8") as f:
         f.write(
-            "export const DATA = "
+            "const DATA = "
             + json.dumps({"nodes": nodes, "edges": edges}, separators=(",", ":"))
         )
         f.close()
